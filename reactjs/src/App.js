222fc956aeb58e06e71677b5975a49a2735f84df
Aulyardha <aulyardhaanindita@gmail.com>
2022-12-13 22:41:59 +0700

    audy

---
+++ reactjs/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./vintage-1817555.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Audyardha <code>Nasywa Andini</code> ANA
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Lihat Disini!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
